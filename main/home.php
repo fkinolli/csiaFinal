db939c0331c8244f47fa9376e88c6e0bff01395c
FK <fkinolli@pinewood-school.gr>
2021-04-18 18:50:06 +0300

    made seperate css files for home and login/signup

--- main/home.php
+++ main/home.php
@@ -3,7 +3,7 @@
   <head>
     <title>Home Page</title>
     <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" href="main.css">
+    <link rel="stylesheet" type="text/css" href="home.css">
   </head>
   <body>
     <h1>Supposed Landing Page</h1>
